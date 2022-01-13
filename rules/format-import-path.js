10b293578553f2b752927eb8ff8de87816f0f2af
AtsushiM <at.mizoue@gmail.com>
2022-01-13 10:19:14 +0900

    update format-import-path: auto formatを追加

--- rules/format-import-path.js
+++ rules/format-import-path.js
@@ -2,25 +2,26 @@ const path = require('path')
 const { replacePaths } = require('../libs/common')
 const { BASE_SCHEMA_PROPERTIES, calculateDomainContext, calculateDomainNode } = require('../libs/common_domain')
 
+const SCHEMA_FORMAT_PROPERTY = { type: 'string', pattern: '^(none|absolute|relative|auto)$', default: 'none'}
 const SCHEMA = [
   {
     type: 'object',
     properties: {
       ...BASE_SCHEMA_PROPERTIES,
       format: {
-        all: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
-        outside: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
-        globalModule: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
-        module: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
-        domain: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
-        lower: { type: 'string', pattern: '^(none|absolute|relative)$', default: 'none'},
+        all: SCHEMA_FORMAT_PROPERTY,
+        outside: SCHEMA_FORMAT_PROPERTY,
+        globalModule: SCHEMA_FORMAT_PROPERTY,
+        module: SCHEMA_FORMAT_PROPERTY,
+        domain: SCHEMA_FORMAT_PROPERTY,
+        lower: SCHEMA_FORMAT_PROPERTY,
       },
     },
     additionalProperties: false,
   }
 ]
 
-const isConvertType = (calcContext, calcDomainNode) => {
+const convertType = (calcContext, calcDomainNode) => {
   const { option: { format: { all, outside, globalModule, module, domain, lower } } } = calcContext
   const { isGlobalModuleImport, isModuleImport, isDomainImport, isLowerImport } = calcDomainNode
 
@@ -43,12 +44,46 @@ const isConvertType = (calcContext, calcDomainNode) => {
   return all || 'none'
 }
 
+const calculateAbsoluteImportPath = ({ importPath, resolvedImportPath }) => {
+  if (importPath[0] !== '.') {
+    return importPath
+  }
+
+  return Object.entries(replacePaths).reduce((prev, [key, values]) => {
+    if (resolvedImportPath === prev) {
+      return values.reduce((p, v) => {
+        if (prev === p) {
+          const regexp = new RegExp(`^${path.resolve(`${process.env.PWD}/${v}`)}(.+)$`)
+
+          if (prev.match(regexp)) {
+            return p.replace(regexp, `${key}/$1`).replace(/(\/)+/g, '/')
+          }
+        }
+
+        return p
+      }, prev)
+    }
+
+    return prev
+  }, resolvedImportPath)
+}
+const calculateRelativeImportPath = ({ importPath, filteredDirs, filteredPaths }) => {
+  // HINT: 相対パスの場合でも、余計にさかのぼっていたりする場合もあるので修正対象とする
+  if (
+    importPath[0] !== '.' &&
+    !importPath.match(new RegExp(`^(${Object.keys(replacePaths).join('|')})`))
+  ) {
+    return importPath
+  }
+
+  return `${filteredDirs.length === 0 ? './' : [...Array(filteredDirs.length)].reduce((prev) => `${prev}../`, '')}${filteredPaths.join('/')}`.replace(/^(.+)\.[a-z0-9]+?$/, '$1')
+}
+
 module.exports = {
   meta: {
     type: 'suggestion',
     messages: {
-      'absolute-import-path': '{{ message }}',
-      'relative-import-path': '{{ message }}',
+      'format-import-path': '{{ message }}',
     },
     fixable: 'code',
     schema: SCHEMA,
@@ -65,68 +100,37 @@ module.exports = {
       ImportDeclaration: (node) => {
         const calcDomainNode = calculateDomainNode(calcContext, node)
         const { importPath } = calcDomainNode
-        const convertType = isConvertType(calcContext, calcDomainNode)
 
-        if (convertType === 'absolute') {
-          if (importPath[0] !== '.') {
-            return
+        const fixedImportPath = (() => {
+          switch (convertType(calcContext, calcDomainNode)) {
+            case 'absolute':
+              return calculateAbsoluteImportPath(calcDomainNode)
+            case 'relative':
+              return calculateRelativeImportPath(calcDomainNode)
+            case 'auto':
+              const absoluted = calculateAbsoluteImportPath(calcDomainNode)
+              const relatived = calculateRelativeImportPath(calcDomainNode)
+
+              // HINT: 記述するdirの数でより近い方を選択する。
+              // 相対・絶対記法が同一の場合、おそらくimport元から距離はあるため、たどりやすくするために絶対記法を選択する
+              return (absoluted.split('/').length <= relatived.split('/').length) ? absoluted : relatived
           }
 
-          const { resolvedImportPath } = calcDomainNode
-          const fixedImportPath = Object.entries(replacePaths).reduce((prev, [key, values]) => {
-            if (resolvedImportPath === prev) {
-              return values.reduce((p, v) => {
-                if (prev === p) {
-                  const regexp = new RegExp(`^${path.resolve(`${process.env.PWD}/${v}`)}(.+)$`)
+          return importPath
+        })()
 
-                  if (prev.match(regexp)) {
-                    return p.replace(regexp, `${key}/$1`).replace(/(\/)+/g, '/')
-                  }
-                }
-
-                return p
-              }, prev)
-            }
-
-            return prev
-          }, resolvedImportPath)
-
+        if (importPath !== fixedImportPath) {
           context.report({
             node,
-            messageId: 'absolute-import-path',
+            messageId: 'format-import-path',
             data: {
-              message: `絶対パス(${fixedImportPath})に修正してください`,
+              message: `${fixedImportPath} に修正してください`,
             },
             fix: (fixer) => fixer.replaceText(
               node,
               context.getSourceCode().getText(node).replace(new RegExp(`from '${importPath}'$`), `from '${fixedImportPath}'`)
             ),
           })
-        } else if (convertType === 'relative') {
-          // HINT: 相対パスの場合でも、余計にさかのぼっていたりする場合もあるので修正対象とする
-          if (
-            importPath[0] !== '.' &&
-            !importPath.match(new RegExp(`^(${Object.keys(replacePaths).join('|')})`))
-          ) {
-            return
-          }
-
-          const { filteredDirs, filteredPaths } = calcDomainNode
-          const fixedImportPath = `${filteredDirs.length === 0 ? './' : [...Array(filteredDirs.length)].reduce((prev) => `${prev}../`, '')}${filteredPaths.join('/')}`.replace(/^(.+)\.[a-z0-9]+?$/, '$1')
-
-          if (importPath !== fixedImportPath) {
-            context.report({
-              node,
-              messageId: 'relative-import-path',
-              data: {
-                message: `相対パス(${fixedImportPath})に修正してください`,
-              },
-              fix: (fixer) => fixer.replaceText(
-                node,
-                context.getSourceCode().getText(node).replace(new RegExp(`from '${importPath}'$`), `from '${fixedImportPath}'`)
-              ),
-            })
-          }
         }
       },
     }
