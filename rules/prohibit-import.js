da0f9baadf7955a8b3903c0a8dccc9d6ce481b14
negiandleek <negiandleek@gmail.com>
2022-02-03 17:27:33 +0900

    fix!(BREAKING CHANGE): エラーメッセージをtargetごとにカスタマイズできるようにSchemaの変更する

--- rules/prohibit-import.js
+++ rules/prohibit-import.js
@@ -1,11 +1,13 @@
 const SCHEMA = [
   {
-    type: 'object',
-    properties: {
-      targets: { type: 'object', default: {} },
-      customMessage: { type: 'string'},
-    },
-    additionalProperties: false,
+    type: 'array',
+    items: {
+      properties: {
+        targets: { type: 'object', default: {} },
+        customMessage: { type: 'string'},
+      },
+      reportMessage: false,
+    }
   }
 ]
 
@@ -20,35 +22,36 @@ module.exports = {
     schema: SCHEMA,
   },
   create(context) {
-    const option = context.options[0]
-    const parsedOption = Object.entries(option.targets)
-    const customMessage = option.customMessage
-
+    const options = context.options[0]
     return {
       ImportDeclaration: (node) => {
-        parsedOption.forEach(([matchText, importNames]) => {
-          if (!node.source.value.match(new RegExp(matchText))) {
-            return
-          }
-
-          const imported = (() => {
-            if (!Array.isArray(importNames)) {
-              return !!importNames
+        options.forEach((option) => {
+          const parsedOption = Object.entries(option.targets)
+          const reportMessage = option.reportMessage
+          parsedOption.forEach(([matchText, importNames]) => {
+            if (!node.source.value.match(new RegExp(matchText))) {
+              return
             }
-            const specifier = node.specifiers.find((s) => s.imported && importNames.includes(s.imported.name))
 
-            return specifier ? specifier.imported.name : false
-          })()
+            const imported = (() => {
+              if (!Array.isArray(importNames)) {
+                return !!importNames
+              }
+              const specifier = node.specifiers.find((s) => s.imported && importNames.includes(s.imported.name))
 
-          if (imported) {
-            context.report({
-              node,
-              messageId: 'prohibit_import',
-              data: {
-                message: customMessage ? customMessage : generateDefaultReportMessage(node.source.value, imported),
-              },
-            });
-          }
+              return specifier ? specifier.imported.name : false
+            })()
+
+            if (imported) {
+              context.report({
+                node,
+                messageId: 'prohibit_import',
+                data: {
+                  message: reportMessage ? reportMessage.replace('$moduleName', node.source.value).replace('$exportName', imported) : generateDefaultReportMessage(node.source.value, imported),
+                },
+              });
+            }
+          })
         })
       },
     }
