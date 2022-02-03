3fa16dd74589eb4a9c1525c9e0b9b101c81a95c9
negiandleek <negiandleek@gmail.com>
2022-02-03 17:25:56 +0900

    test: prohibit-import ruleのSchemaを変更するので、テストのoptionを修正する

--- test/prohibit-import.js
+++ test/prohibit-import.js
@@ -12,74 +12,90 @@ ruleTester.run("prohibit-import-lodash", rule, {
   valid: [
     {
       code: `import _ from 'lodash-es'`,
-      options: [{
-        "targets": {
-          '^lodash$': true,
-        }
-      }]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': true,
+          },
+        }]
+      ]
     },
     {
       code: `import { isEqual } from 'lodash-es'`,
-      options: [{
-        "targets": {
-          '^lodash$': ['isEqual'],
-        }
-      }]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': ['isEqual'],
+          },
+        }]
+      ]
     },
     {
       code: `import { isEqaul } from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': ['isDeepEqual'],
-        }
-      }]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': ['isDeepEqual'],
+          },
+        }]
+      ]
     },
     {
       code: `import _ from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': ['isEqual'],
-        }
-      }]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': ['isEqual'],
+          }
+        }]
+      ]
     }
   ],
   invalid: [
     {
       code: `import _ from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': true,
-        }
-      }],
-      errors: [{message: 'lodash は利用しないでください'}]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': true,
+          }
+        }]
+      ],
+      errors: [{ message: 'lodash は利用しないでください' }]
     },
     {
       code: `import { isEqual } from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': true,
-        }
-      }],
-      errors: [{message: 'lodash は利用しないでください'}]
+      options: [
+        [{
+          "targets": {
+            '^lodash$': true,
+          }
+        }]
+      ],
+      errors: [{ message: 'lodash は利用しないでください' }]
     },
     {
       code: `import { isEqual } from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': ['isEqual'],
-        },
-      }],
+      options: [
+        [{
+          "targets": {
+            '^lodash$': ['isEqual'],
+          }
+        }]
+      ],
       errors: [{message: 'lodash/isEqual は利用しないでください'}]
     },
     {
-      code: `import _ from 'lodash'`,
-      options: [{
-        "targets": {
-          '^lodash$': true,
-        },
-        "customMessage": "must not use lodash"
-      }],
-      errors: [{message: 'must not use lodash'}]
+      code: `import { isEqual } from 'lodash'`,
+      options: [
+        [{
+          "targets": {
+            '^lodash$': ['isEqual'],
+          },
+          "reportMessage": "must not use $moduleName/$exportName"
+        }],
+      ],
+      errors: [{message: 'must not use lodash/isEqual'}]
     }
-  ],
+  ]
 })
