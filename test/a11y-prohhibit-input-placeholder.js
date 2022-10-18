72f6f866291e05f277f1efe6b116862bd7a4c033
AtsushiM <at.mizoue@gmail.com>
2022-10-19 08:42:49 +0900

    SearchInputに対応する

--- test/a11y-prohhibit-input-placeholder.js
+++ test/a11y-prohhibit-input-placeholder.js
@@ -25,6 +25,7 @@ ruleTester.run('a11y-prohibit-input-placeholder', rule, {
     { code: 'const hoge = styled.fieldset``' },
     { code: 'const HogeFieldSet = styled(FieldSet)``' },
     { code: 'const HogeComboBox = styled(ComboBox)``' },
+    { code: 'const HogeSearchInput = styled(SearchInput)``' },
     { code: `<input />` },
     { code: `<textarea />` },
     { code: `<FieldSet />` },
@@ -33,6 +34,9 @@ ruleTester.run('a11y-prohibit-input-placeholder', rule, {
     { code: `<HogeTextarea />` },
     { code: `<FugaFieldSet />` },
     { code: `<CustomComboBox />` },
+    { code: `<SearchInput />` },
+    { code: `<CustomSearchInput tooltipMessage="hoge" />` },
+    { code: `<CustomSearchInput tooltipMessage="hoge" placeholder="fuga" />` },
   ],
   invalid: [
     { code: `import hoge from 'styled-components'`, errors: [ { message: "styled-components をimportする際は、名称が`styled` となるようにしてください。例: `import styled from 'styled-components'`" } ] },
@@ -42,11 +46,19 @@ ruleTester.run('a11y-prohibit-input-placeholder', rule, {
     { code: 'const Hoge = styled(StyledTextarea)``', errors: [ { message: `Hogeを正規表現 "/Textarea$/" がmatchする名称に変更してください` } ] },
     { code: 'const Hoge = styled(FieldSet)``', errors: [ { message: `Hogeを正規表現 "/FieldSet$/" がmatchする名称に変更してください` } ] },
     { code: 'const Hoge = styled(ComboBox)``', errors: [ { message: `Hogeを正規表現 "/ComboBox$/" がmatchする名称に変更してください` } ] },
-    { code: `<input placeholder />`, errors: [ { message: `input にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
-    { code: `<textarea placeholder="hoge" />`, errors: [ { message: `textarea にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
-    { code: `<StyledInput placeholder={any} />`, errors: [ { message: `StyledInput にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
-    { code: `<HogeTextarea placeholder="any" />`, errors: [ { message: `HogeTextarea にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
-    { code: `<HogeFieldSet placeholder="any" />`, errors: [ { message: `HogeFieldSet にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
-    { code: `<HogeComboBox placeholder="any" />`, errors: [ { message: `HogeComboBox にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。検索ボックス等、ヒント用要素の領域が確保できない場合のみ、Tooltipの利用を検討してください (例: '<><Input /><Hint>ヒント</Hint></>', '<Tooltip message=\"ヒント\"><Textarea/></Tooltip>')` } ] },
+    { 
+      code: 'const Hoge = styled(SearchInput)``',
+      errors: [
+        { message: `Hogeを正規表現 "/Input$/" がmatchする名称に変更してください` },
+        { message: `Hogeを正規表現 "/SearchInput$/" がmatchする名称に変更してください` },
+      ],
+    },
+    { code: `<input placeholder />`, errors: [ { message: `input にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><input /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<textarea placeholder="hoge" />`, errors: [ { message: `textarea にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><textarea /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<StyledInput placeholder={any} />`, errors: [ { message: `StyledInput にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><StyledInput /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<HogeTextarea placeholder="any" />`, errors: [ { message: `HogeTextarea にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><HogeTextarea /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<HogeFieldSet placeholder="any" />`, errors: [ { message: `HogeFieldSet にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><HogeFieldSet /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<HogeComboBox placeholder="any" />`, errors: [ { message: `HogeComboBox にはplaceholder属性は設定せず、別途ヒント用要素の利用を検討してください。(例: '<div><HogeComboBox /><Hint>ヒント</Hint></div>')` } ] },
+    { code: `<SearchInput placeholder="any" />`, errors: [ { message: `SearchInput にはplaceholder属性を単独で利用せず、tooltipMessageオプションのみ、もしくはplaceholderとtooltipMessageの併用を検討してください。 (例: '<SearchInput tooltipMessage="ヒント" />', '<SearchInput tooltipMessage={hint} placeholder={hint} />')` } ] },
   ]
 })
