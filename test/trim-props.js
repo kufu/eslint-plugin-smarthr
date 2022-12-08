6f8bc8869bebd569210826956c054248a6d64548
takahirohimi <takahiro.mth.1209@gmail.com>
2022-12-08 18:10:50 +0900

    href以外のテストケースを追加

--- test/trim-props.js
+++ test/trim-props.js
@@ -13,6 +13,9 @@ ruleTester.run('trim-props', rule, {
   valid: [
     { code: '<a href="https://www.google.com">google</a>' },
     { code: '<a href={"https://www.google.com"}>google</a>' },
+    { code: '<img src="/sample.jpg" alt="sample" />' },
+    { code: '<img src={"/sample.jpg"} alt={"sample"} />' },
+    { code: '<div data-spec="info-area">....</div>' },
   ],
   invalid: [
     {
@@ -43,6 +46,32 @@ ruleTester.run('trim-props', rule, {
     {
       code: '<a href={" https://www.google.com "}>google</a>',
       output: '<a href={"https://www.google.com"}>google</a>',
+      errors: [{ message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' }],
+    },
+    {
+      code: '<img src=" /sample.jpg" alt="sample " />',
+      output: '<img src="/sample.jpg" alt="sample" />',
+      errors: [
+        { message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' },
+        { message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' },
+      ],
+    },
+    {
+      code: '<img src={" /sample.jpg"} alt={"sample "} />',
+      output: '<img src={"/sample.jpg"} alt={"sample"} />',
+      errors: [
+        { message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' },
+        { message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' },
+      ],
+    },
+    {
+      code: '<div data-spec=" info-area ">....</div>',
+      output: '<div data-spec="info-area">....</div>',
+      errors: [{ message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' }],
+    },
+    {
+      code: '<div data-spec={" info-area "}>....</div>',
+      output: '<div data-spec={"info-area"}>....</div>',
       errors: [{ message: '属性に設定している文字列から先頭、末尾の空白文字を削除してください' }],
     },
   ],
