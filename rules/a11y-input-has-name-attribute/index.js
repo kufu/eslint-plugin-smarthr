b5d11cbc674f8190ffd2fd54d9d0cd3a413cf814
AtsushiM <at.mizoue@gmail.com>
2022-12-29 17:17:45 +0900

    feat: a11y-input-has-name-attribute rule にname属性のフォーマットをチェックする機能を追加

--- rules/a11y-input-has-name-attribute/index.js
+++ rules/a11y-input-has-name-attribute/index.js
@@ -1,11 +1,13 @@
 const { generateTagFormatter } = require('../../libs/format_styled_components');
 
 const EXPECTED_NAMES = {
-  RadioButton$: 'RadioButton$',
+  'RadioButton$': 'RadioButton$',
   '(i|I)nput$': 'Input$',
   '(t|T)extarea$': 'Textarea$',
   '(s|S)elect$': 'Select$',
-};
+}
+const TARGET_TAG_NAME_REGEX = new RegExp(`(${Object.keys(EXPECTED_NAMES).join('|')})`)
+const INPUT_NAME_REGEX = /^[a-zA-Z0-9_\[\]]+$/
 
 module.exports = {
   meta: {
@@ -20,8 +22,15 @@ module.exports = {
     return {
       ...generateTagFormatter({ context, EXPECTED_NAMES }),
       JSXOpeningElement: (node) => {
-        if (!node.attributes.some((attribute) => attribute?.name?.name === 'name')) {
-          const nodeName = node.name.name || '';
+        const nodeName = node.name.name || '';
+
+        if (!nodeName.match(TARGET_TAG_NAME_REGEX)) {
+          return
+        }
+
+        const nameAttr = node.attributes.find((a) => a?.name?.name === 'name')
+
+        if (!nameAttr) {
           const isRadio =
             nodeName.match(/RadioButton$/) ||
             (nodeName.match(/(i|I)nput$/) && node.attributes.some(
@@ -35,6 +44,18 @@ module.exports = {
               message: `${nodeName} にname属性を指定してください。適切に指定することで${isRadio ? 'グループが確立され、キーボード操作しやすくなる' : 'ブラウザの自動補完が有効化される'}などのメリットがあります。`,
             },
           });
+        } else {
+          const nameValue = nameAttr.value?.value || ''
+
+          if (nameValue && !nameValue.match(INPUT_NAME_REGEX)) {
+            context.report({
+              node,
+              messageId: 'a11y-input-has-name-attribute',
+              data: {
+                message: `${nodeName} のname属性の値(${nameValue})はブラウザの自動補完が適切に行えない可能性があるため ${INPUT_NAME_REGEX.toString()} にmatchするフォーマットで命名してください。`,
+              },
+            });
+          }
         }
       },
     };
--- test/a11y-input-has-name-attribute.js
+++ test/a11y-input-has-name-attribute.js
@@ -30,7 +30,7 @@ ruleTester.run('a11y-input-has-name-attribute', rule, {
     { code: '<textarea name="hoge" />' },
     { code: '<HogeTextarea name="hoge" />' },
     { code: '<select name="hoge" />' },
-    { code: '<Select name="hoge" />' },
+    { code: '<Select name="hoge[0][Fuga]" />' },
   ],
   invalid: [
     { code: `import hoge from 'styled-components'`, errors: [ { message: "styled-components をimportする際は、名称が`styled` となるようにしてください。例: `import styled from 'styled-components'`" } ] },
@@ -42,10 +42,13 @@ ruleTester.run('a11y-input-has-name-attribute', rule, {
     { code: '<Input type="checkbox" />', errors: [ { message: 'Input にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
     { code: '<input type="radio" />', errors: [ { message: 'input にname属性を指定してください。適切に指定することでグループが確立され、キーボード操作しやすくなるなどのメリットがあります。' } ] },
     { code: '<HogeInput type="radio" />', errors: [ { message: 'HogeInput にname属性を指定してください。適切に指定することでグループが確立され、キーボード操作しやすくなるなどのメリットがあります。' } ] },
+    { code: '<HogeInput type="text" />', errors: [ { message: 'HogeInput にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
     { code: '<HogeRadioButton />', errors: [ { message: 'HogeRadioButton にname属性を指定してください。適切に指定することでグループが確立され、キーボード操作しやすくなるなどのメリットがあります。' } ] },
     { code: '<select />', errors: [ { message: 'select にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
     { code: '<HogeSelect />', errors: [ { message: 'HogeSelect にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
     { code: '<textarea />', errors: [ { message: 'textarea にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
     { code: '<HogeTextarea />', errors: [ { message: 'HogeTextarea にname属性を指定してください。適切に指定することでブラウザの自動補完が有効化されるなどのメリットがあります。' } ] },
+    { code: '<input type="radio" name="ほげ" />', errors: [ { message: 'input のname属性の値(ほげ)はブラウザの自動補完が適切に行えない可能性があるため /^[a-zA-Z0-9_\\[\\]]+$/ にmatchするフォーマットで命名してください。' } ] },
+    { code: '<select name="hoge[fuga][0][あいうえお]" />', errors: [ { message: 'select のname属性の値(hoge[fuga][0][あいうえお])はブラウザの自動補完が適切に行えない可能性があるため /^[a-zA-Z0-9_\\[\\]]+$/ にmatchするフォーマットで命名してください。' } ] },
   ],
 });
