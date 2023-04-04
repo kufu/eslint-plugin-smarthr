d2bbeeff4d2f48133b2b44f956985cf18aae9800
Mizoue Atsushi <at.mizoue@gmail.com>
2023-04-04 14:52:19 +0900

    feat: best-practice-for-remote-trigger-action-dialog ルールを追加 (#60)
    * feat: best-practice-for-remote-trigger-action-dialog ルールを追加
    
    * Update README.md

---
+++ rules/best-practice-for-remote-trigger-action-dialog/README.md
@@ -0,0 +1,31 @@
+# smarthr/best-practice-for-remote-trigger-action-dialog
+
+- RemoteDialogTrigger, RemoteTriggerActionDialog のベストプラクティスをチェックするルールです
+  - `id` もしくは `targetId` にリテラルな文字列以外が設定できなくなります
+  - 変数なども使えません
+  - これは対応するTriggerとDialogをわかりやすくするためです（検索などが容易になります）
+
+## rules
+
+```js
+{
+  rules: {
+    'smarthr/best-practice-for-remote-trigger-action-dialog': 'error', // 'warn', 'off'
+  },
+}
+```
+
+## ❌ Incorrect
+
+```js
+<RemoteDialogTrigger targetId={id}>open.</RemoteDialogTrigger>
+<RemoteTriggerActionDialog {...args} id={'hoge'}>content.</RemoteTriggerActionDialog>
+```
+
+## ✅ Correct
+
+
+```js
+<RemoteDialogTrigger targetId="help_dialog">open.</RemoteDialogTrigger>
+<RemoteTriggerActionDialog {...args} id="help_dialog">content.</RemoteTriggerActionDialog>
+```
---
+++ rules/best-practice-for-remote-trigger-action-dialog/index.js
@@ -0,0 +1,41 @@
+const { generateTagFormatter } = require('../../libs/format_styled_components');
+
+const EXPECTED_NAMES = {
+  'RemoteDialogTrigger$': 'RemoteDialogTrigger$',
+  'RemoteTrigger(.+)Dialog$': 'RemoteTrigger(.+)Dialog$',
+}
+
+const REGEX_REMOTE_TRIGGER_DIALOG = /RemoteTrigger(.+)Dialog$/
+const REGEX_REMOTE_DIALOG_TRIGGER = /RemoteDialogTrigger$/
+
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    schema: [],
+  },
+  create(context) {
+    return {
+      ...generateTagFormatter({ context, EXPECTED_NAMES }),
+      JSXOpeningElement: (node) => {
+        const nodeName = node.name.name || '';
+
+        const regexRemoteTriggerDialog = nodeName.match(REGEX_REMOTE_TRIGGER_DIALOG)
+
+        if (regexRemoteTriggerDialog || nodeName.match(REGEX_REMOTE_DIALOG_TRIGGER)) {
+          const attrName = regexRemoteTriggerDialog ? 'id' : 'targetId'
+          const id = node.attributes.find((a) => a.name?.name === attrName)
+
+          if (id.value.type !== 'Literal') {
+            context.report({
+              node: id,
+              message: `${nodeName}の${attrName}属性には直接文字列を指定してください。
+  - 変数などは利用できません（これは関連するTriggerとDialogを検索しやすくするためです）
+  - RemoteTriggerActionDialogはループやDropdown内にTriggerが存在する場合に利用してください
+  - ループやDropdown以外にTriggerが設定されている場合、TriggerAndActionDialogを利用してください`,
+            })
+          }
+        }
+      }
+    }
+  },
+}
