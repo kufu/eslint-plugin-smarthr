cbb2f4a4df31a4ac154ee6719028443504d5b35a
Mizoue Atsushi <at.mizoue@gmail.com>
2023-08-29 10:28:35 +0900

    Update rules/best-practice-for-date/index.js
    Co-authored-by: Yuta Yamamoto <ftbae1@gmail.com>

--- rules/best-practice-for-date/index.js
+++ rules/best-practice-for-date/index.js
@@ -1,4 +1,4 @@
-const MESSAGE_NEW_DATE = `'new Date(arg)' のように引数一つのみの指定でのDate instanceの生成は、実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
+const MESSAGE_NEW_DATE = `'new Date(arg)' のように引数を一つだけ指定したDate instanceの生成は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
  - 'new Date(2022, 12 - 1, 31)' のように数値を個別に指定する
  - dayjsなど、日付系ライブラリを利用する (例:  'dayjs(arg).toDate()')`
 const MESSAGE_PARSE = `Date.parse は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
