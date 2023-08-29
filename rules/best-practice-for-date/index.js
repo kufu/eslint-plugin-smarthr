43b478b64f5fc94b4b869f60cb1dd29ca79e7621
Mizoue Atsushi <at.mizoue@gmail.com>
2023-08-29 10:28:43 +0900

    Update rules/best-practice-for-date/index.js
    Co-authored-by: Yuta Yamamoto <ftbae1@gmail.com>

--- rules/best-practice-for-date/index.js
+++ rules/best-practice-for-date/index.js
@@ -3,7 +3,7 @@ const MESSAGE_NEW_DATE = `'new Date(arg)' のように引数を一つだけ指�
  - dayjsなど、日付系ライブラリを利用する (例:  'dayjs(arg).toDate()')`
 const MESSAGE_PARSE = `Date.parse は実行環境によって結果が異なるため、以下のいずれかの方法に変更してください
  - 'new Date(2022, 12 - 1, 31).getTime()' のように数値を個別に指定する
- - dayjsなど、日付系ライブラリを利用する (例:  'dayjs(arg).valueOf()')`
+ - dayjsなど、日付系ライブラリを利用する (例: 'dayjs(arg).valueOf()')`
 
 module.exports = {
   meta: {
