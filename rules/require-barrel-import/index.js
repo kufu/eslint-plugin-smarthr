20a2156940101a6cba98b58adb873337a830fc8c
Mizoue Atsushi <at.mizoue@gmail.com>
2023-09-20 09:17:12 +0900

    chore: require-barrel-importをリファクタリング (#82)

--- rules/require-barrel-import/index.js
+++ rules/require-barrel-import/index.js
@@ -1,49 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const { replacePaths, rootPath } = require('../../libs/common')
-const calculateAbsoluteImportPath = (source) => {
-  if (source[0] === '/') {
-    return source
-  }
 
-  return Object.entries(replacePaths).reduce((prev, [key, values]) => {
-    if (source === prev) {
-      return values.reduce((p, v) => {
-        if (prev === p) {
-          const regexp = new RegExp(`^${key}(.+)$`)
-
-          if (prev.match(regexp)) {
-            return p.replace(regexp, `${path.resolve(`${process.cwd()}/${v}`)}/$1`)
-          }
-        }
-
-        return p
-      }, prev)
-    }
-
-    return prev
-  }, source)
-}
-const calculateReplacedImportPath = (source) => {
-  return Object.entries(replacePaths).reduce((prev, [key, values]) => {
-    if (source === prev) {
-      return values.reduce((p, v) => {
-        if (prev === p) {
-          const regexp = new RegExp(`^${path.resolve(`${process.cwd()}/${v}`)}(.+)$`)
-
-          if (prev.match(regexp)) {
-            return p.replace(regexp, `${key}/$1`).replace(/(\/)+/g, '/')
-          }
-        }
-
-        return p
-      }, prev)
-    }
-
-    return prev
-  }, source)
-}
-const TARGET_EXTS = ['ts', 'tsx', 'js', 'jsx']
 const SCHEMA = [
   {
     type: 'object',
@@ -72,6 +30,59 @@ const SCHEMA = [
   }
 ]
 
+const entriedReplacePaths = Object.entries(replacePaths)
+const CWD = process.cwd()
+const REGEX_UNNECESSARY_SLASH = /(\/)+/g
+const REGEX_ROOT_PATH = new RegExp(`^${rootPath}/index\.`)
+const REGEX_INDEX_FILE = /\/index\.(ts|js)x?$/
+const TARGET_EXTS = ['ts', 'tsx', 'js', 'jsx']
+
+const calculateAbsoluteImportPath = (source) => {
+  if (source[0] === '/') {
+    return source
+  }
+
+  return entriedReplacePaths.reduce((prev, [key, values]) => {
+    if (source === prev) {
+      const regexp = new RegExp(`^${key}(.+)$`)
+
+      return values.reduce((p, v) => {
+        if (prev === p) {
+          if (prev.match(regexp)) {
+            return p.replace(regexp, `${path.resolve(`${CWD}/${v}`)}/$1`)
+          }
+        }
+
+        return p
+      }, prev)
+    }
+
+    return prev
+  }, source)
+}
+const calculateReplacedImportPath = (source) => {
+  return entriedReplacePaths.reduce((prev, [key, values]) => {
+    if (source === prev) {
+      return values.reduce((p, v) => {
+        if (prev === p) {
+          const regexp = new RegExp(`^${path.resolve(`${CWD}/${v}`)}(.+)$`)
+
+          if (prev.match(regexp)) {
+            return p.replace(regexp, `${key}/$1`).replace(REGEX_UNNECESSARY_SLASH, '/')
+          }
+        }
+
+        return p
+      }, prev)
+    }
+
+    return prev
+  }, source)
+}
+
+const pickImportedName = (s) => s.imported?.name
+const findExistsSync = (p) => fs.existsSync(p)
+
 module.exports = {
   meta: {
     type: 'suggestion',
@@ -81,16 +92,13 @@ module.exports = {
     const option = context.options[0] || {}
     const filename = context.getFilename()
 
-    if ((option.ignores || []).some((i) => !!filename.match(new RegExp(i)))) {
+    if (option.ignores && option.ignores.some((i) => !!filename.match(new RegExp(i)))) {
       return {}
     }
 
-    const dir = (() => {
-      const d = filename.split('/')
-      d.pop()
-
-      return d.join('/')
-    })()
+    let d = filename.split('/')
+    d.pop()
+    const dir = d.join('/')
     const targetPathRegexs = Object.keys(option?.allowedImports || {})
     const targetAllowedImports = targetPathRegexs.filter((regex) => !!filename.match(new RegExp(regex)))
 
@@ -105,7 +113,7 @@ module.exports = {
 
           targetModules.forEach((targetModule) => {
             const allowedModules = allowedOption[targetModule] || true
-            const actualTarget = targetModule[0] !== '.' ? targetModule : path.resolve(`${process.cwd()}/${targetModule}`)
+            const actualTarget = targetModule[0] !== '.' ? targetModule : path.resolve(`${CWD}/${targetModule}`)
             let sourceValue = node.source.value
 
             if (actualTarget[0] === '/') {
@@ -116,21 +124,19 @@ module.exports = {
               return
             }
 
-
             if (!Array.isArray(allowedModules)) {
               isDenyPath = true
               deniedModules.push(true)
             } else {
-              deniedModules.push(node.specifiers.map((s) => s.imported?.name).filter(i => allowedModules.indexOf(i) == -1))
+              deniedModules.push(node.specifiers.map(pickImportedName).filter(i => allowedModules.indexOf(i) == -1))
             }
           })
         })
 
-        if (isDenyPath && deniedModules[0] === true) {
-          return
-        }
-
-        if (!isDenyPath && deniedModules.length === 1 && deniedModules[0].length === 0) {
+        if (
+          isDenyPath && deniedModules[0] === true ||
+          !isDenyPath && deniedModules.length === 1 && deniedModules[0].length === 0
+        ) {
           return
         }
 
@@ -167,15 +173,15 @@ module.exports = {
             break
           }
 
-          barrel = TARGET_EXTS.map((e) => `${sourceValue}/index.${e}`).find((p) => fs.existsSync(p)) || barrel
+          barrel = TARGET_EXTS.map((e) => `${sourceValue}/index.${e}`).find(findExistsSync) || barrel
 
           sources.pop()
           sourceValue = sources.join('/')
         }
 
-        if (barrel && !barrel.match(new RegExp(`^${rootPath}/index\.`))) {
+        if (barrel && !barrel.match(REGEX_ROOT_PATH)) {
           barrel = calculateReplacedImportPath(barrel)
-          const noExt = barrel.replace(/\/index\.(ts|js)x?$/, '')
+          const noExt = barrel.replace(REGEX_INDEX_FILE, '')
           deniedModules = [...new Set(deniedModules.flat())]
 
           context.report({
