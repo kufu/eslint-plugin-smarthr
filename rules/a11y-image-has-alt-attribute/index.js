2a667ae3e7164533e0cfbe81a866c221c1f3f036
AtsushiM <at.mizoue@gmail.com>
2023-10-05 14:16:22 +0900

    feat: a11y-image-has-alt-attributeでaria-describedbyが設定されている場合、エラーにならないように修正

--- rules/a11y-image-has-alt-attribute/index.js
+++ rules/a11y-image-has-alt-attribute/index.js
@@ -16,6 +16,7 @@ const UNEXPECTED_NAMES = {
 const REGEX_IMG = /(img|image)$/i // HINT: Iconは別途テキストが存在する場合が多いためチェックの対象外とする
 
 const findAltAttr = (a) => a.name?.name === 'alt'
+const findAriaDescribedbyAttr = (a) => a.name?.name === 'aria-describedby'
 const findSpreadAttr = (a) => a.type === 'JSXSpreadAttribute'
 const isWithinSvgJsxElement = (node) => {
   if (
@@ -31,7 +32,9 @@ const isWithinSvgJsxElement = (node) => {
 const MESSAGE_NOT_EXIST_ALT = `画像にはalt属性を指定してください。
  - コンポーネントが画像ではない場合、img or image を末尾に持たない名称に変更してください。
  - ボタンやリンクの先頭・末尾などに設置するアイコンとしての役割を持つ画像の場合、コンポーネント名の末尾を "Icon" に変更してください。
- - SVG component の場合、altを属性として受け取れるようにした上で '<svg role="img" aria-label={alt}>' のように指定してください。`
+ - SVG component の場合、altを属性として受け取れるようにした上で '<svg role="img" aria-label={alt}>' のように指定してください。
+ - 文字情報が多い場合や画像の前後の画像と同じ内容を設定したい場合などは aria-describedby属性を利用することもできます。
+  - aria-describedby属性を利用する場合でもalt属性を併用することができます。`
 const MESSAGE_NULL_ALT = `画像の情報をテキストにした代替テキスト（'alt'）を設定してください。
  - 装飾目的の画像など、alt属性に指定すべき文字がない場合は背景画像にすることを検討してください。`
 
@@ -67,6 +70,7 @@ module.exports = {
 
             if (!alt) {
               if (
+                !node.attributes.find(findAriaDescribedbyAttr) &&
                 (
                   matcher.input !== 'image' ||
                   !isWithinSvgJsxElement(node.parent)
--- test/a11y-image-has-alt-attribute.js
+++ test/a11y-image-has-alt-attribute.js
@@ -15,7 +15,9 @@ const ruleTester = new RuleTester({
 const messageNotExistAlt = `画像にはalt属性を指定してください。
  - コンポーネントが画像ではない場合、img or image を末尾に持たない名称に変更してください。
  - ボタンやリンクの先頭・末尾などに設置するアイコンとしての役割を持つ画像の場合、コンポーネント名の末尾を "Icon" に変更してください。
- - SVG component の場合、altを属性として受け取れるようにした上で '<svg role="img" aria-label={alt}>' のように指定してください。`
+ - SVG component の場合、altを属性として受け取れるようにした上で '<svg role="img" aria-label={alt}>' のように指定してください。
+ - 文字情報が多い場合や画像の前後の画像と同じ内容を設定したい場合などは aria-describedby属性を利用することもできます。
+  - aria-describedby属性を利用する場合でもalt属性を併用することができます。`
 const messageNullAlt = `画像の情報をテキストにした代替テキスト（'alt'）を設定してください。
  - 装飾目的の画像など、alt属性に指定すべき文字がない場合は背景画像にすることを検討してください。`
 
@@ -37,6 +39,8 @@ ruleTester.run('a11y-image-has-alt-attribute', rule, {
     { code: '<HogeImg alt="hoge" />' },
     { code: '<HogeImage alt="hoge" />' },
     { code: '<HogeIcon />' },
+    { code: '<HogeImage aria-describedby="hoge" />' },
+    { code: '<HogeImage aria-describedby="hoge" alt="fuga" />' },
     { code: '<svg><image /></svg>' },
     { code: '<AnyImg {...hoge} />', options: [{ checkType: 'smart' }] },
   ],
