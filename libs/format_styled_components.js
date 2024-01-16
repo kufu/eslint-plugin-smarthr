689d7da9e899b2801ae2dfdfd465f6cfcc277e85
Mizoue Atsushi <at.mizoue@gmail.com>
2024-01-16 16:49:47 +0900

    fix: import内で型の場合はtype kindを設定することでasでの命名縛りを回避できるように修正 (#102)

--- libs/format_styled_components.js
+++ libs/format_styled_components.js
@@ -27,13 +27,14 @@ const generateTagFormatter = ({ context, EXPECTED_NAMES, UNEXPECTED_NAMES }) => 
   }) : []
 
 
-  const checkImportedNameToLocalName = (node, base, extended, mode) => {
+  const checkImportedNameToLocalName = (node, base, extended, isImport) => {
     entriesesTagNames.forEach(([b, e]) => {
       if (base.match(b) && !extended.match(e)) {
         context.report({
           node,
-          message: `${extended}を正規表現 "${e.toString()}" がmatchする名称に変更してください。
- - ${base}が型の場合、'${mode} type { ${base} as ${extended} }' もしくは '${mode} { type ${base} as ${extended} }' のように明示的に型であることを宣言してください。名称変更が不要になります`,
+          message: `${extended}を正規表現 "${e.toString()}" がmatchする名称に変更してください。${isImport ? `
+ - ${base}が型の場合、'import type { ${base} as ${extended} }' もしくは 'import { type ${base} as ${extended} }' のように明示的に型であることを宣言してください。名称変更が不要になります
+` : ''}`,
         });
       }
     })
@@ -46,7 +47,7 @@ const generateTagFormatter = ({ context, EXPECTED_NAMES, UNEXPECTED_NAMES }) => 
       if (node.importKind !== 'type') {
         node.specifiers.forEach((s) => {
           if (s.importKind !== 'type' && s.imported && s.imported.name !== s.local.name) {
-            checkImportedNameToLocalName(node, s.imported.name, s.local.name, 'import')
+            checkImportedNameToLocalName(node, s.imported.name, s.local.name, true)
           }
         })
       }
