994c04027892a5fe50fb71ba8b5941401f12c02c
Mizoue Atsushi <at.mizoue@gmail.com>
2024-01-23 08:33:28 +0900

    feat: a11y-prohibit-useless-sectioning-fragmentを追加する (#106)

---
+++ rules/a11y-prohibit-useless-sectioning-fragment/index.js
@@ -0,0 +1,74 @@
+const { generateTagFormatter } = require('../../libs/format_styled_components')
+
+const EXPECTED_NAMES = {
+  'Article$': '(Article)$',
+  'Aside$': '(Aside)$',
+  'Nav$': '(Nav)$',
+  'Section$': '(Section)$',
+  'Center$': '(Center)$',
+  'Reel$': '(Reel)$',
+  'Sidebar$': '(Sidebar)$',
+  'Stack$': '(Stack)$',
+}
+
+const UNEXPECTED_NAMES = EXPECTED_NAMES
+
+const BARE_SECTIONING_TAG_REGEX = /^(article|aside|nav|section)$/
+const SECTIONING_REGEX = /((A(rticle|side))|Nav|Section)$/
+const SECTIONING_FRAGMENT_REGEX = /^SectioningFragment$/
+const LAYOUT_REGEX = /((C(ent|lust)er)|Reel|Sidebar|Stack)$/
+const AS_REGEX = /^(as|forwardedAs)$/
+
+const includeSectioningAsAttr = (a) => a.name?.name.match(AS_REGEX) && a.value.value.match(BARE_SECTIONING_TAG_REGEX)
+
+const searchSectioningFragment = (node) => {
+  switch (node.type) {
+    case 'JSXElement':
+      return node.openingElement.name?.name.match(SECTIONING_FRAGMENT_REGEX) ? node.openingElement : null
+    case 'Program':
+      return null
+  }
+
+  return searchSectioningFragment(node.parent)
+}
+
+const SCHEMA = []
+
+module.exports = {
+  meta: {
+    type: 'problem',
+    schema: SCHEMA,
+  },
+  create(context) {
+    return {
+      ...generateTagFormatter({ context, EXPECTED_NAMES, UNEXPECTED_NAMES }),
+      JSXOpeningElement: (node) => {
+        const name = node.name?.name || ''
+        let hit = null
+        let asAttr = null
+
+        if (name.match(SECTIONING_REGEX)) {
+          hit = true
+        } else {
+          asAttr = name.match(LAYOUT_REGEX) && node.attributes.find(includeSectioningAsAttr)
+
+          if (asAttr) {
+            hit = true
+          }
+        }
+
+        if (hit) {
+          result = searchSectioningFragment(node.parent.parent)
+
+          if (result) {
+            context.report({
+              node: result,
+              message: `無意味なSectioningFragmentが記述されています。子要素である<${name}${asAttr ? ` ${asAttr.name.name}="${asAttr.value.value}"` : ''}>で問題なくセクションは設定されているため、このSectioningFragmentは削除してください`
+            })
+          }
+        }
+      },
+    }
+  },
+}
+module.exports.schema = SCHEMA
---
+++ test/a11y-prohibit-useless-sectioning-fragment.js
@@ -0,0 +1,31 @@
+const rule = require('../rules/a11y-prohibit-useless-sectioning-fragment')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+})
+
+const error = (tag) => `無意味なSectioningFragmentが記述されています。子要素である${tag}で問題なくセクションは設定されているため、このSectioningFragmentは削除してください`
+
+ruleTester.run('a11y-prohibit-useless-sectioning-fragment', rule, {
+  valid: [
+    { code: `<SectioningFragment>hoge</SectioningFragment>` },
+    { code: `<Section><SectioningFragment>hoge</SectioningFragment></Section>` },
+    { code: `<AnyAside><SectioningFragment>hoge</SectioningFragment></AnyAside>` },
+    { code: `<AnyNav>hoge</AnyNav>` },
+    { code: `<AnyArticle>hoge</AnyArticle>` },
+  ],
+  invalid: [
+    { code: `<SectioningFragment><AnySection /></SectioningFragment>`, errors: [ { message: error('<AnySection>') } ] },
+    { code: `<SectioningFragment><AnyAside>hoge</AnyAside></SectioningFragment>`, errors: [ { message: error('<AnyAside>') } ] },
+    { code: `<SectioningFragment><HogeStack as="aside">hoge</HogeStack></SectioningFragment>`, errors: [ { message: error('<HogeStack as="aside">') } ] },
+    { code: `<SectioningFragment><HogeReel forwardedAs="nav">hoge</HogeReel></SectioningFragment>`, errors: [ { message: error('<HogeReel forwardedAs="nav">') } ] },
+  ]
+})
