e33552569d38d2c7dd17f1a118fe7e9ac09df99f
Mizoue Atsushi <at.mizoue@gmail.com>
2024-03-18 08:40:51 +0900

    chore: すべてのルールをoffの状態にする (#122)

--- index.js
+++ index.js
@@ -23,23 +23,6 @@ function generateRulesMap() {
   return rulesMap;
 }
 
-const DISAPPROVE_RULE_NAMES = [
-  'a11y-form-control-in-form', // formを使用することの是非について議論中のため
-
-  // ルールが動作するために設定が必要なものはrecommendedに含めない
-  'format-import-path',
-  'format-translate-component',
-  'jsx-start-with-spread-attributes',
-  'no-import-other-domain',
-  'prohibit-file-name',
-  'prohibit-import',
-  'prohibit-path-within-template-literal',
-  'require-declaration',
-  'require-export',
-  'require-import',
-]
-const DISAPPROVE_RULE_NAMES_REGEX = new RegExp(`^(${DISAPPROVE_RULE_NAMES.join('|')})$`)
-
 function generateRecommendedConfig(rules) {
   let config = {
     plugins: ['smarthr'],
@@ -47,9 +30,7 @@ function generateRecommendedConfig(rules) {
   };
 
   for (let ruleName of Object.keys(rules)) {
-    if (!DISAPPROVE_RULE_NAMES_REGEX.test(ruleName)) {
-      config.rules[`smarthr/${ruleName}`] = 'off';
-    }
+    config.rules[`smarthr/${ruleName}`] = 'off';
   }
 
   return config;
