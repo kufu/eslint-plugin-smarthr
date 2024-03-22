8c5ab74b8b6f22f601fe8da69bd8ec7895065569
AtsushiM <at.mizoue@gmail.com>
2024-03-22 17:18:33 +0900

    feat: a11y-heading-in-sectioning-contentでHeadingを持たないSectioning Contentを検知するように修正

--- rules/a11y-heading-in-sectioning-content/index.js
+++ rules/a11y-heading-in-sectioning-content/index.js
@@ -52,11 +52,11 @@ const bareTagRegex = /^(article|aside|nav|section)$/
 const modelessDialogRegex = /ModelessDialog$/
 const layoutComponentRegex = /((C(ent|lust)er)|Reel|Sidebar|Stack)$/
 const asRegex = /^(as|forwardedAs)$/
+const ignoreCheckParentTypeRegex = /^(Program|ExportNamedDeclaration)$/
+const noHeadingTagNamesRegex = /^(span|legend)$/
+const ignoreHeadingCheckParentTypeRegex = /^(Program|ExportNamedDeclaration)$/
 
-const includeSectioningAsAttr = (a) => a.name?.name.match(asRegex) && a.value.value.match(bareTagRegex)
-
-const noHeadingTagNames = ['span', 'legend']
-const ignoreHeadingCheckParentType = ['Program', 'ExportNamedDeclaration']
+const includeSectioningAsAttr = (a) => a.name?.name.match(asRegex) && bareTagRegex.test(a.value.value)
 
 const headingMessage = `smarthr-ui/Headingと紐づく内容の範囲（アウトライン）が曖昧になっています。
  - smarthr-uiのArticle, Aside, Nav, SectionのいずれかでHeadingコンポーネントと内容をラップしてHeadingに対応する範囲を明確に指定してください。
@@ -102,8 +102,8 @@ const searchBubbleUp = (node) => {
   if (
     node.type === 'Program' ||
     node.type === 'JSXElement' && node.openingElement.name.name && (
-      node.openingElement.name.name.match(sectioningRegex) ||
-      node.openingElement.name.name.match(layoutComponentRegex) && node.openingElement.attributes.some(includeSectioningAsAttr)
+      sectioningRegex.test(node.openingElement.name.name) ||
+      layoutComponentRegex.test(node.openingElement.name.name) && node.openingElement.attributes.some(includeSectioningAsAttr)
     )
   ) {
     return node
@@ -111,15 +111,112 @@ const searchBubbleUp = (node) => {
 
   if (
     // Headingコンポーネントの拡張なので対象外
-    node.type === 'VariableDeclarator' && ignoreHeadingCheckParentType.includes(node.parent.parent?.type) && node.id.name.match(declaratorHeadingRegex) ||
-    node.type === 'FunctionDeclaration' && ignoreHeadingCheckParentType.includes(node.parent.type) && node.id.name.match(declaratorHeadingRegex) ||
+    node.type === 'VariableDeclarator' && node.parent.parent?.type.match(ignoreHeadingCheckParentTypeRegex) && declaratorHeadingRegex.test(node.id.name) ||
+    node.type === 'FunctionDeclaration' && ignoreHeadingCheckParentTypeRegex.test(node.parent.type) && declaratorHeadingRegex.test(node.id.name) ||
     // ModelessDialogのheaderにHeadingを設定している場合も対象外
-    node.type === 'JSXAttribute' && node.name.name === 'header' && node.parent.name.name.match(modelessDialogRegex)
+    node.type === 'JSXAttribute' && node.name.name === 'header' && modelessDialogRegex.test(node.parent.name.name)
   ) {
     return null
   }
 
   return searchBubbleUp(node.parent)
+}
+const searchBubbleUpSections = (node) => {
+  switch (node.type) {
+    case 'Program':
+      // rootまで検索した場合は確定でエラーにする
+      return null
+    case 'VariableDeclarator':
+      // SectioningContent系コンポーネントの拡張の場合は対象外
+      if (ignoreCheckParentTypeRegex.test(node.parent.parent?.type) && sectioningRegex.test(node.id.name)) {
+        return node
+      }
+
+      break
+    case 'FunctionDeclaration':
+    case 'ClassDeclaration':
+      // SectioningContent系コンポーネントの拡張の場合は対象外
+      if (ignoreCheckParentTypeRegex.test(node.parent.type) && sectioningRegex.test(node.id.name)) {
+        return node
+      }
+
+      break
+  }
+
+  return searchBubbleUpSections(node.parent)
+}
+const searchChildren = (n) => {
+  switch (n.type) {
+    case 'BinaryExpression':
+    case 'Identifier':
+    case 'JSXEmptyExpression':
+    case 'JSXText':
+    case 'Literal':
+    case 'VariableDeclaration':
+      // これ以上childrenが存在しないため終了
+      return false
+    case 'JSXAttribute':
+      return n.value ? searchChildren(n.value) : false
+    case 'LogicalExpression':
+      return searchChildren(n.right)
+    case 'ArrowFunctionExpression':
+      return searchChildren(n.body)
+    case 'MemberExpression':
+      return searchChildren(n.property)
+    case 'ReturnStatement':
+    case 'UnaryExpression':
+      return searchChildren(n.argument)
+    case 'ChainExpression':
+    case 'JSXExpressionContainer':
+      return searchChildren(n.expression)
+    case 'BlockStatement': {
+      return forInSearchChildren(n.body)
+    }
+    case 'ConditionalExpression': {
+      return searchChildren(n.consequent) || searchChildren(n.alternate)
+    }
+    case 'CallExpression': {
+      return forInSearchChildren(n.arguments)
+    }
+    case 'JSXFragment':
+      break
+    case 'JSXElement': {
+      const name = n.openingElement.name.name || ''
+
+      if (
+        sectioningRegex.test(name) ||
+        layoutComponentRegex.test(name) && n.openingElement.attributes.some(includeSectioningAsAttr)
+      ) {
+        return false
+      } else if (
+        (
+          headingRegex.test(name) &&
+          !n.openingElement.attributes.find(findTagAttr)?.value.value.match(noHeadingTagNamesRegex)
+        ) ||
+        forInSearchChildren(n.openingElement.attributes)
+      ) {
+        return true
+      }
+
+      break
+    }
+  }
+
+  return n.children ? forInSearchChildren(n.children) : false
+}
+
+const forInSearchChildren = (ary) => {
+  let r = false
+
+  for (const i in ary) {
+    r = searchChildren(ary[i])
+
+    if (r) {
+      break
+    }
+  }
+
+  return r
 }
 
 const findTagAttr = (a) => a.name?.name == 'tag'
@@ -151,15 +248,15 @@ module.exports = {
             message,
           })
         // Headingに明示的にtag属性が設定されており、それらが span or legend の場合はHeading扱いしない
-        } else if (elementName.match(headingRegex)) {
+        } else if (headingRegex.test(elementName)) {
           const tagAttr = node.attributes.find(findTagAttr)
 
-          if (!noHeadingTagNames.includes(tagAttr?.value.value)) {
+          if (!tagAttr?.value.value.match(noHeadingTagNamesRegex)) {
             const result = searchBubbleUp(node.parent)
             let hit = false
 
             if (result) {
-              if (elementName.match(pageHeadingRegex)) {
+              if (pageHeadingRegex.test(elementName)) {
                 h1s.push(node)
 
                 if (h1s.length > 1) {
@@ -181,7 +278,7 @@ module.exports = {
                   node,
                   message: rootHeadingMessage,
                 })
-              } else if (sections.find((s) => s === result)) {
+              } else if (sections.includes(result)) {
                 hit = true
                 context.report({
                   node,
@@ -198,6 +295,18 @@ module.exports = {
                 message: noTagAttrMessage,
               })
             }
+          }
+        } else if (!node.selfClosing) {
+          const isSection = sectioningRegex.test(elementName)
+          const layoutSectionAsAttr = !isSection && layoutComponentRegex.test(elementName) ? node.attributes.find(includeSectioningAsAttr) : null
+
+          if ((isSection || layoutSectionAsAttr) && !searchBubbleUpSections(node.parent.parent) && !forInSearchChildren(node.parent.children)) {
+            context.report({
+              node,
+              message: `${isSection ? elementName : `<${elementName} ${layoutSectionAsAttr.name.name}="${layoutSectionAsAttr.value.value}">`} はHeading要素を含んでいません。
+ - SectioningContentはHeadingを含むようにマークアップする必要があります
+ - Headingにするべき適切な文字列が存在しない場合、 ${isSection ? `${elementName} は削除するか、SectioningContentではない要素に差し替えてください` : `${layoutSectionAsAttr.name.name}="${layoutSectionAsAttr.value.value}"を削除、もしくは別の要素に変更してください`}`,
+            })
           }
         }
       },
