a0f6ba096a7c197b444a1424cfa702883096ef0b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2024-04-09 10:43:40 +0900

    Update test/a11y-prohibit-input-maxlength-attribute.js

--- test/a11y-prohibit-input-maxlength-attribute.js
+++ test/a11y-prohibit-input-maxlength-attribute.js
@@ -13,7 +13,7 @@ const ruleTester = new RuleTester({
 })
 
 const expectedErrorMessage = (elName) => `${elName}にmaxLength属性を設定しないでください。
-- maxLength属性がついた要素に、テキストをペーストすると、maxLength属性の値を超えた範囲が意図せず切り捨てられてしまう場合がある
+- maxLength属性がついた要素に、テキストをペーストすると、maxLength属性の値を超えた範囲が意図せず切り捨てられてしまう場合があります
 - 以下のいずれかの方法で修正をおこなってください
   - 方法1: pattern属性とtitle属性を組み合わせ、form要素でラップする
   - 方法2: JavaScriptを用いたバリデーションを実装する`
