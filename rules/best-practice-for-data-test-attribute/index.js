3740420dc0e45c5eb6d71ae0491be06d2a041b8e
atzzCokeK <atsushi1230.sdi@gmail.com>
2024-06-10 15:03:02 +0900

    feat: best-practice-for-data-test-attributeを追加

---
+++ rules/best-practice-for-data-test-attribute/index.js
@@ -0,0 +1,36 @@
+const SCHEMA = []
+
+const prohibitAttributies = [
+  "data-spec",
+  "data-testid"
+]
+
+/**
+ * @type {import('@typescript-eslint/utils').TSESLint.RuleModule<''>}
+ */
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    schema: SCHEMA,
+  },
+  create(context) {
+    return {
+      JSXAttribute: (node) => {
+        const hit = prohibitAttributies.find((attr) => attr === node.name.name)
+
+        if (hit) {
+          context.report({
+            node,
+            message: `テストのために要素を指定するために、${hit} 属性を利用するのではなく、他の方法で要素を指定することを検討してください。
+ - 方法1: click_link, click_button等を利用したりすることで、利用しているテスト環境に準じた方法で要素を指定することを検討してください。
+   - 参考(Testing Library): https://testing-library.com/docs/queries/about
+   - 参考(Capybara): https://rubydoc.info/github/jnicklas/capybara/Capybara/Node/Finders
+ - 方法2: テスト環境のメソッド等で要素が指定できない場合はrole属性、name属性、id属性等を利用した方法で要素を指定することを検討してください。
+ - 方法3: 上記の方法でも要素が指定できない場合は、'eslint-disable-next-line' 等を利用して、このルールを無効化してください。`,
+          });
+        }
+      },
+    }
+  },
+}
+module.exports.schema = SCHEMA
